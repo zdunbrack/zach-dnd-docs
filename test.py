8803472c12fd7471dcfb8961ee65b3d16bbb13db
Zach Dunbrack <zachdunbrack@gmail.com>
2025-01-30 18:07:04 -0500

    Quartz sync: Jan 30, 2025, 6:07 PM

--- test.py
+++ test.py
@@ -4,4 +4,5 @@ import os
 os.system("cp -r /c/Users/zachd/Documents/aemora ./content")
 for root, subdirs, files in os.walk("."):
   for file in files:
-    print(file)
+    if "public: true" not in open(file).read():
+      print("file: " + str(file) + " is not public")
