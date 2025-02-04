9cfbae5ae1da40d35bb9417705086766b8aba04c
Zach Dunbrack <zachdunbrack@gmail.com>
2025-02-03 22:34:15 -0500

    sync updates

--- sync.py
+++ sync.py
@@ -8,8 +8,8 @@ os.system("cp -r /c/Users/zachd/Documents/aemora/* ./content")
 for root, subdirs, files in os.walk('./content'):
   for file in files:
     fullPath = os.path.join(root, file)
-    if ".md" in file and """---
+    if ".md" in file and """
 tags:
   - publish
----""" not in open(fullPath).read():
+""" not in open(fullPath).read():
       os.remove(fullPath)
