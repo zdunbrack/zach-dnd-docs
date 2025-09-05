ab47486793160fea0df971f6f40fc1437878e739
Zach Dunbrack <zachdunbrack@gmail.com>
2025-09-05 01:00:21 -0400

    sync update

--- sync.py
+++ sync.py
@@ -8,7 +8,7 @@ os.system("cp -r /c/Users/zachd/Documents/aemora/* ./content")
 for root, subdirs, files in os.walk('./content'):
   for file in files:
     fullPath = os.path.join(root, file)
-    with open(fullPath, 'rw', encoding='utf-8') as file:
+    with open(fullPath, 'r', encoding='utf-8') as file:
       content = open(fullPath).read()
     if ".md" in file and """
 tags:
